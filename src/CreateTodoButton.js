33b418e8047c785c6eab77a1df23f909c6085454
EAMR22 <elvis1042.rodriguez@gmail.com>
2023-11-02 10:30:40 -0600

    Eventos con onClick y onChange

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,16 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton() {
     return(
-        <button className="CreateTodoButton">+</button>
+        <button 
+            className="CreateTodoButton" 
+            onClick = {(event) => { // A la hora de persionar un click ocurre un evento.
+                    console.log('le diste click');
+                    console.log(event);
+                    console.log(event.target);
+                    console.log(event.target.value);
+                }
+            }
+        >+</button>
     );
 }
 
