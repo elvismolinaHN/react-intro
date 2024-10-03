d60ef0d6b7b74e4dc107227d4436d67fe5774911
elvishn <elvis1042.rodriguez@gmail.com>
2024-10-03 17:01:17 -0600

    Eventos con onClick y onChange

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -5,6 +5,11 @@ function TodoSearch() {
         <input 
             placeholder="Cortar cebolla"
             className="TodoSearch"
+            onChange={(event) => {
+                console.log(event);
+                console.log(event.target); // Trae el html que se esta llamando
+                console.log(event.target.value); // Detecta cada silaba que esta escribiendo el usuario
+            }}
         ></input>
     );
 }
