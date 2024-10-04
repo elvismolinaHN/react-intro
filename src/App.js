015525c38f2ef6c7ed934c651b4530b877a7af2a
elvishn <elvis1042.rodriguez@gmail.com>
2024-10-03 20:42:56 -0600

    Funcionalidad de contando todos por medio de estados derivados

--- src/App.js
+++ src/App.js
@@ -3,24 +3,35 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
 
 const defaultTodos = [
   {text: 'Cortar cebolla', completed: true},
   {text: 'Hice la tarea', completed: false},
   {text: 'Jugue futbol', completed: false},
-  {text: 'Complete el curso', completed: true}
+  {text: 'Complete el curso', completed: true},
+  {text: 'Usando estados derivados', completed: true}
 ];
 
-// El React.Fragment envuelve los componentes.
-// El .map crea un nuevo array a partir de haber recorrido uno.
-// La propiedad key hace que envie un texto diferente en los componentes TodoItem.
-
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos); // React.useState(defaultTodos) es el estado inicial
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length; // Filtra los elementos que ya fueron completados
+  const totalTodos = todos.length; // Es un estado derivado porque toma funcionalidades de otro estado
+
+  console.log('Los usuarios buscan todos de: ' + searchValue);
+
   return (
     <> 
-
-      <TodoCounter completed={6} total={9}/>
-      <TodoSearch />
+      <TodoCounter 
+        completed={completedTodos} 
+        total={totalTodos}
+      />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
         {defaultTodos.map(todo => (
